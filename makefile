5ce5e18dbafc1015fa1ce3568eb50ef8c7c6cc84
Yeray Diaz Diaz <yeraydiazdiaz@gmail.com>
2022-04-02 11:24:52 +0100

    Move to flit + pyproject.toml

--- makefile
+++ makefile
@@ -1,25 +1,6 @@
-.PHONY: pin-requirements install-dev package release-test release-pypi
-.SILENT: pin-requirements install-dev package release-test release-pypi
-
-pin-requirements:
-	pip-compile requirements/base.in --output-file requirements/base.txt
+.PHONY: install-dev
+.SILENT: install-dev
 
 install-dev:
+	pip install -U pip
 	pip install -e .[dev]
-
-package:
-	rm -fr dist/*
-	python setup.py sdist
-	python setup.py bdist_wheel --universal
-
-release-test: package
-	@echo "Are you sure you want to release to test.pypi.org? [y/N]" && \
-		read ans && \
-		[ $${ans:-N} = y ] && \
-		twine upload --repository testpypi dist/*
-
-release-pypi: package
-	@echo "Are you sure you want to release to pypi.org? [y/N]" && \
-		read ans && \
-		[ $${ans:-N} = y ] && \
-		twine upload dist/*
--- noxfile.py
+++ noxfile.py
@@ -4,34 +4,21 @@ import nox
 nox.options.stop_on_first_error = True
 nox.options.reuse_existing_virtualenvs = True
 
-source_files = ("src/envfiles", "tests", "setup.py", "noxfile.py")
+source_files = ("src/envfiles", "tests", "noxfile.py")
 
 
 @nox.session
 def lint(session):
-    session.install(
-        "--upgrade", "autoflake", "black", "flake8", "isort", "seed-isort-config"
-    )
-
+    session.install("-e", ".[dev]")
     session.run("autoflake", "--in-place", "--recursive", *source_files)
     session.run("seed-isort-config", "--application-directories=src/envfiles")
-    session.run("isort", "--project=envfiles", "--recursive", "--apply", *source_files)
+    session.run("isort", "--project=envfiles", *source_files)
     session.run("black", "--target-version=py36", *source_files)
 
 
 @nox.session
 def check(session):
-    session.install(
-        "--upgrade",
-        "black",
-        "flake8",
-        "flake8-bugbear",
-        "flake8-comprehensions",
-        "flake8-pie",
-        "isort",
-        "mypy",
-    )
-
+    session.install("-e", ".[dev]")
     session.run("black", "--check", "--diff", "--target-version=py36", *source_files)
     session.run("flake8", *source_files)
     session.run("mypy", "src/envfiles")
@@ -44,3 +31,30 @@ def check(session):
 def test(session):
     session.install("-e", ".[test]")
     session.run("python", "-m", "pytest", *session.posargs)
+
+
+@nox.session
+def publish_test(session):
+    _publish(session, "testpypi")
+
+
+@nox.session
+def publish(session):
+    _publish(session, None)
+
+
+def _publish(session, repository):
+    session.install("flit")
+    url = "pypi.org"
+    if repository == "testpypi":
+        url = f"test.{url}"
+
+    confirm = input(f"Are you sure you want to release to {url}? ")
+    if confirm.lower().strip() not in ("y", "yes"):
+        session.error("Aborting as requested")
+
+    args = ["flit", "publish"]
+    if repository is not None:
+        args += ["--repository", repository]
+
+    session.run(*args)
